fd4e8fc587c56c9a4f31b6d2fc1f171e5b13fbb8
chugunov <cugunovroman03@gmail.com>
2026-01-18 17:59:27 -0800

    add register and generate tokens service

--- src/api/auth/auth.module.ts
+++ src/api/auth/auth.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+
+import { getJwtConfig } from '../../config';
+
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
 
 @Module({
+  imports: [
+    PassportModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: getJwtConfig,
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
--- src/api/auth/auth.service.ts
+++ src/api/auth/auth.service.ts
@@ -1,12 +1,31 @@
 import { ConflictException, Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { hash } from 'argon2';
+import ms, { StringValue } from 'ms';
 
+import { User } from '../../../generated/prisma-client/client';
 import { PrismaService } from '../../infra/prisma/prisma.service';
 
 import { RegisterDto } from './dto';
+import { JwtPayload } from './interfaces';
 
 @Injectable()
 export class AuthService {
-  public constructor(private readonly prismaService: PrismaService) {}
+  private readonly JWT_ACCESS_TOKEN_EXPIRES_IN: StringValue;
+  private readonly JWT_REFRESH_TOKEN_EXPIRES_IN: StringValue;
+  public constructor(
+    private readonly prismaService: PrismaService,
+    private readonly configService: ConfigService,
+    private readonly jwtService: JwtService,
+  ) {
+    this.JWT_ACCESS_TOKEN_EXPIRES_IN =
+      this.configService.getOrThrow<StringValue>('JWT_TOKEN_EXPIRES_IN');
+    this.JWT_REFRESH_TOKEN_EXPIRES_IN =
+      this.configService.getOrThrow<StringValue>(
+        'JWT_REFRESH_TOKEN_EXPIRES_IN',
+      );
+  }
 
   public async register(dto: RegisterDto) {
     const { name, email, password } = dto;
@@ -19,14 +38,33 @@ export class AuthService {
       throw new ConflictException('User with this email already exists');
     }
 
+    const hashedPassword = await hash(password);
+
     const user = await this.prismaService.user.create({
       data: {
         name,
         email,
-        password,
+        password: hashedPassword,
       },
     });
 
-    return user;
+    return this.generateTokens(user);
+  }
+
+  private async generateTokens(user: User) {
+    const payload: JwtPayload = { id: user.id };
+
+    const refreshTokenExpiresInMs = new Date(
+      Date.now() + ms(this.JWT_REFRESH_TOKEN_EXPIRES_IN),
+    );
+
+    const accessToken = await this.jwtService.signAsync(payload, {
+      expiresIn: this.JWT_ACCESS_TOKEN_EXPIRES_IN,
+    });
+
+    const refreshToken = await this.jwtService.signAsync(payload, {
+      expiresIn: this.JWT_REFRESH_TOKEN_EXPIRES_IN,
+    });
+    return { accessToken, refreshToken, refreshTokenExpiresInMs };
   }
 }
